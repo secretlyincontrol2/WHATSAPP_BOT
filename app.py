6885bcd336afa545a7dd45736bf634ebefe842ea
BU22-2130 <bu22-2130@babcock.edu>
2026-02-17 10:36:45 +0100

    Remove file logging to fix permission error

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("whatsapp_bot.log"),
         logging.StreamHandler()
     ]
 )
